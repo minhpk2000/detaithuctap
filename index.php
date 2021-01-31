7099fc1be96490b4b120316418f13c7061c8efd6
minhpk <minhdaica2000hihi@gmail.com>
2021-01-31 23:12:14 +0700

    minh index

---
+++ index.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+
+<body>
+<div class="sum">
+<div class="top">
+	
+	<div id="top1" ><img src="anh/banner.jpg"></div>
+	<div id="top2">
+		
+			<div id="menu2">
+			  <div class="menu">
+        <ul>
+            <li><a href="#">Trang Chủ</a></li>
+            <li>
+                <a href="#">Đồng hồ</a>
+                <ul>
+                    <li><a href="#">Đồng hồ nam</a></li>
+                    <li><a href="#">Đồng hồ nữ</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Phụ kiện</a>
+                <ul>
+                    <li><a href="#">Phụ kiện nam</a></li>
+                    <li><a href="#">Phụ kiện nữ</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Thương hiệu </a>
+			 <ul>
+                    <li><a href="#">Khám phá </a></li>
+                    <li><a href="#">Về chúng tôi</a></li>
+				 <li><a href="#">Tìm hiểu thêm</a></li>
+                </ul>
+			</li>
+			<li><a href="#">Tài khoản </a>
+			 <ul>
+                    <li><a href="#">Đăng kí</a></li>
+                    <li><a href="#">Đăng nhập</a></li>
+                </ul>
+			</li>
+			<li><a href="#">Giỏ hàng </a></li>
+        </ul>
+    </div>			
+</div>
+	
+		
+</div>
+		
+</div>
+	<div class="clr"></div>
+	<div class="center">
+	<div id="center1"><img src="anh/center1.JPG"></div>
+	<div id="nut"><a href="#"><img src="anh/nut1.JPG"></a> <a href="#"> <img src="anh/nut1.JPG"></a> </div>
+
+	<div id="center2">
+		<p><a href="#"><img src="anh/dn1.JPG"></a> <a href="#"><img src="anh/dn3.JPG"></p>
+		<p><a href="#"><img src="anh/dn2.JPG"></a> <a href="#"><img src="anh/dn4.JPG"></p>
+		
+		
+	</div>	
+		
+		<div id="center3">
+			<p>MEN'S BEST SELLERS</p>
+			<p><a href="#">Khám phá thêm</a></p>
+			<p> <img src="anh/dhn1.JPG"> <img src="anh/dhn2.JPG"> <img src="anh/dhn3.JPG"> <img src="anh/dhn4.JPG"></p>
+		
+		</div>
+		
+		<div id="center4">
+			<p>WOMEN'S BEST SELLERS</p>
+			<p><a href="#">Khám phá thêm</a></p>
+			<p> <img src="anh/dhnu1.JPG"> <img src="anh/dhnu2.JPG"> <img src="anh/dhnu3.JPG"> <img src="anh/dhnu4.JPG"></p>
+		
+		</div>
+		<div id="center5">
+			<img src="anh/gt.JPG">
+		
+		</div>
+		<div id="center6">
+			<div id="center6_left"><p>Giới thiệu </p>
+				<p>1.Mỗi chiếc Đồng hồ Curnon được thiết kế phát triển bởi chất xám và nỗ lực của những con người Việt Nam, mang theo cảm hứng và khát khao của tuổi trẻ.</p>
+				<p>2.Đồng hồ Curnon sở hữu tính năng chống nước từ 3ATM cho đến 10ATM, giúp bạn tự tin hoạt động ngay trong những môi trường hay thời tiết không thuận lợi.</p>
+				<p>3.Mặt kính Sapphire hạn chế tối đa xước xát, xóa bỏ nỗi lo va đập, giúp mặt đồng hồ luôn sáng bóng..</p>
+				<p>4.Thay dây đồng hồ, biến đổi phong cách thời trang của bạn mọi lúc mọi nơi trong tích tắc</p>
+			<br><br></div>
+			<div id="center6_right"><img src="anh/gtsp.JPG"></div>
+		
+		</div>
+		<div id="center7">
+			<img src="anh/chot1.JPG">
+		
+		</div>
+	</div>
+	<div class="bottom">
+	<img src="anh/cho2.JPG"><img src="anh/cho3.JPG">
+	
+	</div>
+	
+	
+	
+	
+</div>
+</body>
+</html>
